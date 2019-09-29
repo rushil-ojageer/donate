90d273597bc31fb900e914b168c48bd1bc82fe89
Rushil <rushilojageer@gmail.com>
2019-09-29 20:00:19 +0200

    Fixed deloy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-docker build -t rushilojageer/donate-gateway:latest -t rushilojageer/donate-gateway:$SHA -f ./Backend/Donate/Donate/Gateway/Donate.Gateway/Dockerfile ./donate-gateway
-docker build -t rushilojageer/donate-charityapi:latest -t rushilojageer/donate-charityapi:$SHA -f ./Backend/Donate/Donate/CharityService/Donate.CharityService.API/Dockerfile ./donate-charityapi
-docker build -t rushilojageer/donate-donorapi:latest -t rushilojageer/donate-donorapi:$SHA -f ./Backend/Donate/Donate/DonorService/Donate.DonorService.API/Dockerfile ./donate-donorapi
-docker build -t rushilojageer/donate-donorworker:latest -t rushilojageer/donate-donorworker:$SHA -f ./Backend/Donate/Donate/DonorService/Donate.DonorService.IntegrationWorker/Dockerfile ./donate-donorworker
-docker build -t rushilojageer/donate-fundapi:latest -t rushilojageer/donate-fundapi:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.API/Dockerfile ./donate-fundapi
-docker build -t rushilojageer/donate-fundworker:latest -t rushilojageer/donate-fundworker:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.IntegrationWorker/Dockerfile ./donate-fundworker
-docker build -t rushilojageer/donate-transactionfeed:latest -t rushilojageer/donate-transactionfeed:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.IntegrationWorker/Dockerfile ./donate-transactionfeed
-docker build -t rushilojageer/donate-transactionprocessor:latest -t rushilojageer/donate-transactionprocessor:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.TransactionProcessor/Dockerfile ./donate-transactionprocessor
+docker build -t rushilojageer/donate-gateway:latest -t rushilojageer/donate-gateway:$SHA -f ./Backend/Donate/Donate/Gateway/Donate.Gateway/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-charityapi:latest -t rushilojageer/donate-charityapi:$SHA -f ./Backend/Donate/Donate/CharityService/Donate.CharityService.API/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-donorapi:latest -t rushilojageer/donate-donorapi:$SHA -f ./Backend/Donate/Donate/DonorService/Donate.DonorService.API/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-donorworker:latest -t rushilojageer/donate-donorworker:$SHA -f ./Backend/Donate/Donate/DonorService/Donate.DonorService.IntegrationWorker/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-fundapi:latest -t rushilojageer/donate-fundapi:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.API/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-fundworker:latest -t rushilojageer/donate-fundworker:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.IntegrationWorker/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-transactionfeed:latest -t rushilojageer/donate-transactionfeed:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.IntegrationWorker/Dockerfile ./Backend/Donate/Donate/
+docker build -t rushilojageer/donate-transactionprocessor:latest -t rushilojageer/donate-transactionprocessor:$SHA -f ./Backend/Donate/Donate/FundService/Donate.FundService.TransactionProcessor/Dockerfile ./Backend/Donate/Donate/
 
 docker push rushilojageer/donate-gateway:latest
 docker push rushilojageer/donate-charityapi:latest
@@ -24,13 +24,3 @@ docker push rushilojageer/donate-fundapi:$SHA
 docker push rushilojageer/donate-fundworker:$SHA
 docker push rushilojageer/donate-transactionfeed:$SHA
 docker push rushilojageer/donate-transactionprocessor:$SHA
-
-kubectl apply -f K8S
-kubectl set image deployments/gateway-deployment client=rushilojageer/donate-gateway:$SHA
-kubectl set image deployments/charityapi-deployment server=rushilojageer/donate-charityapi:$SHA
-kubectl set image deployments/donorapi-deployment client=rushilojageer/donate-donorapi:$SHA
-kubectl set image deployments/donorworker-deployment worker=rushilojageer/donate-donorworker:$SHA
-kubectl set image deployments/fundapi-deployment server=rushilojageer/donate-fundapi:$SHA
-kubectl set image deployments/fundworker-deployment client=rushilojageer/donate-fundworker:$SHA
-kubectl set image deployments/transactionprocessor-deployment worker=rushilojageer/donate-transactionprocessor:$SHA
-kubectl set image deployments/transactionfeed-deployment server=rushilojageer/donate-transactionfeed:$SHA
